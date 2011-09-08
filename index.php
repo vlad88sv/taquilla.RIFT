33cab3b7d93d0c569dc3619e178792274f5efa13
Vladimir Hidalgo <vlad@todosv.com>
2011-09-07 21:46:12 -0600

    No contar el dinero cancelado de cafeteria

--- index.php
+++ index.php
@@ -103,14 +103,14 @@ if ($handle = opendir('./PDF')) {
 <p>Máximo historico en días <?php echo $dia; ?>: <b>$<?php echo $maximo; ?></b></p>
 <hr />
 <?php
-$c = "SELECT FORMAT((SELECT COALESCE(SUM(precio_grabado*cantidad),0) FROM `cafeteria_transacciones` WHERE DATE(`cafeteria_transacciones`.`fecha`) = '".$fecha_sql."') + COALESCE(SUM(precio_grabado),0)+COALESCE((SELECT SUM(`eventos`.`precio_evento` + `eventos`.`precio_comida` + `eventos`.`precio_cafeteria`) FROM `eventos` WHERE DATE(`eventos`.`fecha_vendido`)= '".$fecha_sql."'),0),2) AS total FROM tickets WHERE DATE(fecha_vendido) =  '".$fecha_sql."'";
+$c = "SELECT FORMAT((SELECT COALESCE(SUM(precio_grabado*cantidad),0) FROM `cafeteria_transacciones` WHERE `cafeteria_transacciones`.`cancelado`=0 AND DATE(`cafeteria_transacciones`.`fecha`) = '".$fecha_sql."') + COALESCE(SUM(precio_grabado),0)+COALESCE((SELECT SUM(`eventos`.`precio_evento` + `eventos`.`precio_comida` + `eventos`.`precio_cafeteria`) FROM `eventos` WHERE DATE(`eventos`.`fecha_vendido`)= '".$fecha_sql."'),0),2) AS total FROM tickets WHERE DATE(fecha_vendido) =  '".$fecha_sql."'";
 $r = db_consultar($c);
 $f = mysql_fetch_assoc($r);
 ?>
 <p>Dinero en caja + bouchers: <b>$<?php echo $f['total']; ?></b></p>
 <p class="diminuto">"Total t" es la suma del ingreso registrado en el dia.</p>
 <?php
-$c = "SELECT FORMAT((SELECT COALESCE(SUM(precio_grabado*cantidad),0) FROM `cafeteria_transacciones` WHERE DATE(`cafeteria_transacciones`.`fecha`) = '".$fecha_sql."') + COALESCE(SUM(precio_grabado),0)+COALESCE((SELECT SUM(`eventos`.`precio_evento` + `eventos`.`precio_comida` + `eventos`.`precio_cafeteria`) FROM `eventos` WHERE DATE(`eventos`.`fecha_evento`)= '".$fecha_sql."'),0),2) AS total FROM tickets WHERE DATE(fecha_juego) =  '".$fecha_sql."'";
+$c = "SELECT FORMAT((SELECT COALESCE(SUM(precio_grabado*cantidad),0) FROM `cafeteria_transacciones` WHERE `cafeteria_transacciones`.`cancelado`=0 AND DATE(`cafeteria_transacciones`.`fecha`) = '".$fecha_sql."') + COALESCE(SUM(precio_grabado),0)+COALESCE((SELECT SUM(`eventos`.`precio_evento` + `eventos`.`precio_comida` + `eventos`.`precio_cafeteria`) FROM `eventos` WHERE DATE(`eventos`.`fecha_evento`)= '".$fecha_sql."'),0),2) AS total FROM tickets WHERE DATE(fecha_juego) =  '".$fecha_sql."'";
 $r = db_consultar($c);
 $f = mysql_fetch_assoc($r)
 ?>
